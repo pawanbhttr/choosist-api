33f69b8984a77c6ea849407910f86b92fd8e768d
Pawan Bhattarai <pbhattarai@devfinity.io>
2022-06-09 13:03:44 +0545

    Default Route updated

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('/', app, document);
 
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
